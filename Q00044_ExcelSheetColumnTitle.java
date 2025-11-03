691646db0b5ac766b348984d974ed12effd69315
Susan <susanlan5252@gmail.com>
2025-11-03 19:34:34 +0800

    feat: add Q00044

---
+++ Q00044_ExcelSheetColumnTitle.java
@@ -0,0 +1,53 @@
+/*
+Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet.
+
+For example:
+
+A -> 1
+B -> 2
+C -> 3
+...
+Z -> 26
+AA -> 27
+AB -> 28 
+...
+ 
+
+Example 1:
+
+Input: columnNumber = 1
+Output: "A"
+Example 2:
+
+Input: columnNumber = 28
+Output: "AB"
+Example 3:
+
+Input: columnNumber = 701
+Output: "ZY"
+ 
+
+Constraints:
+
+1 <= columnNumber <= 231 - 1
+ */
+
+
+class Solution {
+    public String convertToTitle(int columnNumber) {
+        int rest = columnNumber;
+        StringBuilder ans = new StringBuilder();
+        while (rest > 0){
+            rest--; //0-base
+            int r = rest % 26;
+            ans.append((char)(r + 'A'));
+            rest /= 26;
+                        
+        }
+        return ans.reverse().toString();
+    }
+}
+
+
+//28/26 = 1 ...2 AB
+//701/26 = 26...25
