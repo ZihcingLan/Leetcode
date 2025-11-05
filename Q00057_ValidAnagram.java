330f89bd82c550956fd68bc1a12112fa25981d8b
Susan <susanlan5252@gmail.com>
2025-11-05 23:58:02 +0800

    feat: add Q00057

---
+++ Q00057_ValidAnagram.java
@@ -0,0 +1,75 @@
+/*
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+
+Output: true
+
+Example 2:
+
+Input: s = "rat", t = "car"
+
+Output: false
+
+ 
+
+Constraints:
+
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.
+ */
+
+import java.util.*;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for (int i = 0; i < t.length(); i++){
+            char c = t.charAt(i);
+            if (!map.containsKey(c)){
+                return false;
+            }
+            map.put(c, map.getOrDefault(c, 0) - 1);
+            if (map.get(c) <= 0){
+                map.remove(c);
+            }
+        }
+        return map.isEmpty();
+    }
+}
+
+/*
+Note:
+
+別人的寫法:可以用一個array去存，利用26字母排列的特性去計算次數
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        
+        int[] freq = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
+            freq[t.charAt(i) - 'a']--;
+        }
+        
+        for (int i = 0; i < freq.length; i++) {
+            if (freq[i] != 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
+ */
