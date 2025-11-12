e084175042db9af356235b0e94a0b25b91c84a81
14L <susanlan5252@gmail.com>
2025-11-13 06:12:37 +0800

    feat: add Q02095 (LinkedLIst)

---
+++ Q02095_DeleteTheMiddleNodeOfALinkedList.java
@@ -0,0 +1,76 @@
+/*
+You are given the head of a linked list. Delete the middle node, and return the head of the modified linked list.
+
+The middle node of a linked list of size n is the ⌊n / 2⌋th node from the start using 0-based indexing, where ⌊x⌋ denotes the largest integer less than or equal to x.
+
+For n = 1, 2, 3, 4, and 5, the middle nodes are 0, 1, 1, 2, and 2, respectively.
+ 
+
+Example 1:
+
+
+Input: head = [1,3,4,7,1,2,6]
+Output: [1,3,4,1,2,6]
+Explanation:
+The above figure represents the given linked list. The indices of the nodes are written below.
+Since n = 7, node 3 with value 7 is the middle node, which is marked in red.
+We return the new list after removing this node. 
+Example 2:
+
+
+Input: head = [1,2,3,4]
+Output: [1,2,4]
+Explanation:
+The above figure represents the given linked list.
+For n = 4, node 2 with value 3 is the middle node, which is marked in red.
+Example 3:
+
+
+Input: head = [2,1]
+Output: [2]
+Explanation:
+The above figure represents the given linked list.
+For n = 2, node 1 with value 1 is the middle node, which is marked in red.
+Node 0 with value 2 is the only node remaining after removing node 1.
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [1, 105].
+1 <= Node.val <= 105
+ */
+
+
+/*時間複雜度:O(n) -> fast一次跑兩步，while 只會跑約 n/2 次，成長率與 n 成正比。
+  空間複雜度:O(1) -> 僅使用固定數量指標變數
+記得邊界檢查，if 長度n=1，n/2個=1，因此要回傳空linkedlist
+*/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+        if (head.next == null){
+            head = null;
+            return head;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode pre = slow;
+        while (fast != null && fast.next !=null){
+            pre = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        pre.next = slow.next;
+        
+        return head;
+    }
+}
