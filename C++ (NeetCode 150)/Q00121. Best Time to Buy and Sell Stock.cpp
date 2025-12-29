e2999fc4d05c09ad8398a5d817b75112c41810b1
14L <susanlan5252@gmail.com>
2025-12-29 12:02:29 +0800

    feat: add Q00121

---
+++ C++ (NeetCode 150)/Q00121. Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,58 @@
+/*
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+ 
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+Example 2:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+ 
+
+Constraints:
+
+1 <= prices.length <= 105
+0 <= prices[i] <= 104
+*/
+
+/*
+Solution: Find the min buy price and the max profit
+Time: O(n) -> In the worstest case, need to traverse the vector once O(n)
+Space: O(1) -> No extra space needed except from constant variables.
+*/
+
+#include <iostream>
+#include <vector>
+#include <math.h>
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+
+        int maxProfit = 0;
+        int minPrice = prices[0];
+        for (int i = 1; i < prices.size(); i++){
+
+            if (prices[i] < minPrice){
+                minPrice = prices[i];
+            } else { //This can be modified:  maxProfit = max(maxProfit, prices[i] - price);
+                int dif = prices[i] - minPrice;
+                if (dif > maxProfit){
+                    maxProfit = dif;
+                }
+            }
+        }
+        return maxProfit;
+    }
+};
