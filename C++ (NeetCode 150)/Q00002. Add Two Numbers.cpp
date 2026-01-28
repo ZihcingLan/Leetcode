c8085b94ca319dca7dbd35623881cb72ac077259
14L <susanlan5252@gmail.com>
2026-01-28 12:43:20 +0800

    practice Q00002

--- C++ (NeetCode 150)/Q00002. Add Two Numbers.cpp
+++ C++ (NeetCode 150)/Q00002. Add Two Numbers.cpp
@@ -65,3 +65,42 @@ public:
 };
 
 Complete **
+
+
+//C
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+//不把dummy malloc一個空間再heap
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+    struct ListNode dummy;  //store in stack
+    dummy.val = 0;
+    dummy.next = NULL;
+
+    struct ListNode* cur = &dummy;
+    int carry = 0;
+    while (l1 || l2 || carry){
+        int sum = carry;
+        if (l1){
+            sum += l1->val;
+            l1 = l1->next;
+        }
+        if (l2){
+            sum += l2->val;
+            l2 = l2->next;
+        }
+        carry = sum / 10;
+        struct ListNode* newNode = (struct ListNode*)malloc(sizeof(struct ListNode));
+        newNode->val = sum % 10;
+        newNode->next = NULL;
+
+        cur->next = newNode;
+        cur = cur->next;
+    }
+    return dummy.next;// dummy是物件
+}
